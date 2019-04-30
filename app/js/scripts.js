cd448bd20d6d616166364551e95fac54fbfb62fa
xkored <xkored@gmail.com>
2019-04-30 17:57:35 +0400

    start

---
+++ app/js/scripts.js
@@ -0,0 +1,46 @@
+$(function() {
+  $('.portfolio__slider').owlCarousel({
+      nav: true,
+      loop: false,
+      dots: false,
+      margin: 20,
+      rtl: true,
+      navText: ["<img src='img/icon__arrow-prev.svg'>", "<img src='img/icon__arrow-next.svg'>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 1
+        },
+        560 : {
+            items: 1
+        },
+        768 : {
+            items: 2
+        },
+        1040 : {
+            items: 3
+        }
+      }
+	});
+	// tabs
+  $('.profile__tabs > div').on('click', function() {
+    $('.profile__descr-item').removeClass('active');
+    $('.profile__tabs > div').removeClass('active');
+    $('.profile__descr-item[data-profile="' + $(this).attr('data-profile') + '"]').addClass('active');
+    $(this).addClass('active');
+  });
+  //Закрываем AjaxForm popup после успешной отправки
+  // $(document).on('af_complete', function(event,res) {
+  //   if(res.success) parent.$.fancybox.close();
+  // });
+  $('#wrapper').fullpage({
+    anchors: ['1section', '2section', '3section', '4section', '5section', '6section'],
+    navigation: true,
+    navigationPosition: 'right'
+  });
+  $('.offer__mouse').on('click', function() {
+    $("#wrapper").fullpage.moveTo($(this).index() + 1);
+  });
+});
