72fa35b9e2abbe344261093b97cfee0c3ff53725
xkored <xkored@gmail.com>
2019-05-13 12:38:55 +0400

    created adaptive + finished contacts page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -8,13 +8,16 @@ $(function() {
       navText: ["<img src='img/icon__arrow-prev.svg'>", "<img src='img/icon__arrow-next.svg'>"],
       responsive : {
         0   : {
-            items: 1
+            items: 1,
+            rtl: false,
         },
-        380 : {
-            items: 1
+        430 : {
+            items: 1,
+            rtl: false,
         },
         560 : {
-            items: 1
+            items: 2,
+            rtl: false,
         },
         768 : {
             items: 2
@@ -39,9 +42,29 @@ $(function() {
     anchors: ['first-section', 'second-section', 'third-section', 'fourth-section', 'fifth-section', 'sixth-section'],
     navigation: true,
     navigationPosition: 'right',
-    menu: '.nav'
+    menu: '.nav',
+    responsiveWidth: '1024'
   });
+  //Кнопка скролла на вторую секцию
   $('.offer__mouse').on('click', function() {
     $("#wrapper").fullpage.moveTo($(this).index() + 1);
+  });
+  //Кнопка наверх
+  $('.btn_top').hide();
+  var top_show = 150; // В каком положении полосы прокрутки начинать показ кнопки "Наверх"
+  var delay = 500; // Задержка прокрутки
+  $(document).ready(function() {
+    $(window).scroll(function () { // При прокрутке попадаем в эту функцию
+      /* В зависимости от положения полосы прокрукти и значения top_show, скрываем или открываем кнопку "Наверх" */
+      if ($(this).scrollTop() > top_show) $('.btn_top').fadeIn();
+      else $('.btn_top').fadeOut();
+    });
+    $('.btn_top').click(function (e) { // При клике по кнопке "Наверх" попадаем в эту функцию
+      /* Плавная прокрутка наверх */
+      e.preventDefault();
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
   });
 });
